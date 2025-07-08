557d0b3cbef3d6e330f4339d5e5f5b8cc2e7be2e
Shiyi Yin <yinshiyi2020@gmail.com>
2025-07-08 15:01:20 -0700

    polish test script

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
-python prepare_vcc_submission.py \
-  --pert_counts ./pert_counts_Validation.csv \
-  --gene_names ./gene_names.csv \
-  --training_adata ./adata_Training.h5ad \
-  --output_h5ad ./example.h5ad
-cell-eval prep -i ./example.h5ad --genes ./gene_names.csv
+# pyenv virtualenv 3.10.13 vcc-env
+# pyenv activate vcc-env
+# pip install -r requirements.txt
+
+data=/Users/yins/Desktop/vcc_data/
+python main.py \
+  --pert_counts $data/pert_counts_Validation.csv \
+  --gene_names $data/gene_names.csv \
+  --training_adata $data/adata_Training.h5ad \
+  --output_h5ad $data/example2.h5ad
+cell-eval prep -i $data/example.h5ad --genes $data/gene_names.csv
